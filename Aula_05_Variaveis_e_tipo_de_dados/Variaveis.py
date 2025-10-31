b0b12a92aa9c06fd7aa84de280bc75cfaebff3bc
Rafael T Santos <texera09@gmail.com>
2025-10-31 16:02:22 -0300

    adiciona var do tipo booleana

--- Aula_05_Variaveis_e_tipo_de_dados/Variaveis.py
+++ Aula_05_Variaveis_e_tipo_de_dados/Variaveis.py
@@ -1,3 +1,4 @@
 nome = "Rafael" # variável do tipo string (texto), sempre entre aspas ('' ou "")
 idade = 39      # var do tipo inteiro (núm sem casas decimais)
 altura = 1.70   # var do tipo float (núm com casas decimais)
+dev = True      # var do tipo booleana, valores lógicos (true/false)
