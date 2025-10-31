38d18acedb96c85ed9a29a26b619ae41b0460cf0
Rafael T Santos <texera09@gmail.com>
2025-10-31 17:45:55 -0300

    Implementa entrada de dados e conversão de tipos

--- Aula_05_Variaveis_e_tipo_de_dados/variaveis_interativas.py
+++ Aula_05_Variaveis_e_tipo_de_dados/variaveis_interativas.py
@@ -1,3 +1,5 @@
 nome = input ("Digite seu nome: ")              # entrada de texto
 idade = int(input ("Digite sua idade: "))       #entrada de texto convertida pra int
 altura = float(input("Digite sua altura: "))    # entrada convertida para float
+
+print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}m.")
