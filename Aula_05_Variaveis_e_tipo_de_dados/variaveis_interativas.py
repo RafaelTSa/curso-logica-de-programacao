634d7f18a20a2f9954490e2bd832c3ee601c35d4
Rafael T Santos <texera09@gmail.com>
2025-10-31 17:34:53 -0300

    variavel altura com entrada convertida para float

--- Aula_05_Variaveis_e_tipo_de_dados/variaveis_interativas.py
+++ Aula_05_Variaveis_e_tipo_de_dados/variaveis_interativas.py
@@ -1,2 +1,3 @@
-nome = input ("Digite seu nome: ") # entrada de texto
-idade = int(input ("Digite sua idade: ")) #entrada de texto convertida pra int
+nome = input ("Digite seu nome: ")              # entrada de texto
+idade = int(input ("Digite sua idade: "))       #entrada de texto convertida pra int
+altura = float(input("Digite sua altura: "))    # entrada convertida para float
