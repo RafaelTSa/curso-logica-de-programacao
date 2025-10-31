1546a1d16de5772cdc9a2950462971b063270286
Rafael T Santos <texera09@gmail.com>
2025-10-31 17:06:09 -0300

    adiciona print para mostrar nome, idade, altura, conforme variaveis

--- Aula_05_Variaveis_e_tipo_de_dados/Variaveis.py
+++ Aula_05_Variaveis_e_tipo_de_dados/Variaveis.py
@@ -2,3 +2,5 @@ nome = "Rafael" # variável do tipo string (texto), sempre entre aspas ('' ou ""
 idade = 39      # var do tipo inteiro (núm sem casas decimais)
 altura = 1.70   # var do tipo float (núm com casas decimais)
 dev = True      # var do tipo booleana, valores lógicos (true/false)
+
+print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}")
