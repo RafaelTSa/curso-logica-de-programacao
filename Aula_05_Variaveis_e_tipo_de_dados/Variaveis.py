635f12dbec36ff0abb5db12681d78a00dc62bacd
Rafael T Santos <texera09@gmail.com>
2025-10-31 17:12:29 -0300

    Adiciona comentario ao comando print

--- Aula_05_Variaveis_e_tipo_de_dados/Variaveis.py
+++ Aula_05_Variaveis_e_tipo_de_dados/Variaveis.py
@@ -3,4 +3,4 @@ idade = 39      # var do tipo inteiro (núm sem casas decimais)
 altura = 1.70   # var do tipo float (núm com casas decimais)
 dev = True      # var do tipo booleana, valores lógicos (true/false)
 
-print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}")
+print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}")        # mostra uma mensagem com as variaveis nome, idade, altura.
