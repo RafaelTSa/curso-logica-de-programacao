6c917f611ea4840f275a5e32b80c2ae54dcbbb47
Rafael T Santos <texera09@gmail.com>
2025-11-06 07:29:19 -0300

    print comparando dois numero com o operador diferente

--- Aula_06_Operadores_matemáticos/operadores_comparacao.py
+++ Aula_06_Operadores_matemáticos/operadores_comparacao.py
@@ -1,3 +1,4 @@
 # Operadores de comparação: ==, !=, >, <, >=, <=
 print(2 == 2) # compara dois números com operador de igualdade " == " retorna "true"
 print(2 == 3) #compara dois numeros com operador de igauldade "==" retorna "false"
+print(4 != 2) #compara dois numeros com operador de diferente "!=" retorna "true"
