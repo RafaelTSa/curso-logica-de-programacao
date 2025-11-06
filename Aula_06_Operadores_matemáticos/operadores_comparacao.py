6bc1d718700179e65747c315044e6aefe457a647
Rafael T Santos <texera09@gmail.com>
2025-11-06 11:24:33 -0300

    print da comparação entre dois numeros retornando false

--- Aula_06_Operadores_matemáticos/operadores_comparacao.py
+++ Aula_06_Operadores_matemáticos/operadores_comparacao.py
@@ -2,3 +2,4 @@
 print(2 == 2) # compara dois números com operador de igualdade " == " retorna "true"
 print(2 == 3) #compara dois numeros com operador de igauldade "==" retorna "false"
 print(4 != 2) #compara dois numeros com operador de diferente "!=" retorna "true"
+print(4 != 4) #compara dois numeros com operador de diferente "!=" retorna "false"
