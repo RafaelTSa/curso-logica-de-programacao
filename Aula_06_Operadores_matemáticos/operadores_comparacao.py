5b34d8894cb4f9c176f0b195fd0428d9837f8dbc
Rafael T Santos <texera09@gmail.com>
2025-11-06 07:16:34 -0300

    adiciona print que compara dosi numero e retorna false

--- Aula_06_Operadores_matemáticos/operadores_comparacao.py
+++ Aula_06_Operadores_matemáticos/operadores_comparacao.py
@@ -1,2 +1,3 @@
 # Operadores de comparação: ==, !=, >, <, >=, <=
 print(2 == 2) # compara dois números com operador de igualdade " == " retorna "true"
+print(2 == 3) #compara dois numeros com operador de igauldade "==" retorna "false"
