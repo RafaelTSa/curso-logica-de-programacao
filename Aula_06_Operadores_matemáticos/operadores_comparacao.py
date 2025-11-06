dba02b6d3ccadd81c6615fb7d7510b5f6830b45e
Rafael T Santos <texera09@gmail.com>
2025-11-06 11:40:41 -0300

    printa na tela a comparação entre dois números retorna false

--- Aula_06_Operadores_matemáticos/operadores_comparacao.py
+++ Aula_06_Operadores_matemáticos/operadores_comparacao.py
@@ -1,5 +1,6 @@
 # Operadores de comparação: ==, !=, >, <, >=, <=
-print(2 == 2) # compara dois números com operador de igualdade " == " retorna "true"
+print(2 == 2) #compara dois números com operador de igualdade " == " retorna "true"
 print(2 == 3) #compara dois numeros com operador de igauldade "==" retorna "false"
 print(4 != 2) #compara dois numeros com operador de diferente "!=" retorna "true"
+print(4 != 4) #compara dois numeros com operador de diferente "!=" retorna "false"
 print(4 != 4) #compara dois numeros com operador de diferente "!=" retorna "false"
