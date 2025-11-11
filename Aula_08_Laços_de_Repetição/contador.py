9dc818ac0d88eb8ce5197cf84b17b4590798c15f
Rafael T Santos <texera09@gmail.com>
2025-11-11 16:18:50 -0300

    adiciona laço de repetição ao contador.py

--- Aula_08_Laços_de_Repetição/contador.py
+++ Aula_08_Laços_de_Repetição/contador.py
@@ -1,1 +1,8 @@
 # Contando de 1 a 100 com o FOR
+
+# [1, 2, 3, 4, 5, 6, 7, 8,  ... 100 ]
+for numero in range(1, 101):
+    print(numero)
+
+# O range (1, 6) gera os números de 1 a 5, o último número da range não é incluido
+# [1, 2, 3, 4, 5]
