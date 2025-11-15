95ca3c146e3afb8e99bd924ffd1002c6adc6e2cd
Rafael T Santos <texera09@gmail.com>
2025-11-15 19:54:11 -0300

    adiciona numeros_pares.py

---
+++ Aula_08_Laços_de_Repetição/numeros_pares.py
@@ -0,0 +1,9 @@
+# Números Pares:
+
+# range (início, fim, passo):
+# início: Onde a sequência começa.
+# fim: O número onde a sequência termina.
+# passo: O valor de incremento entre cada número.
+
+for numero in range (0, 21, 2): # função range com 3 argumentos (início, meio, fim)
+    print(numero)
