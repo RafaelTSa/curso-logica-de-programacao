bc49e30eb30556631e4653015919448c83b4b9a3
Rafael T Santos <texera09@gmail.com>
2025-11-15 18:34:55 -0300

    adiciona senha.py

---
+++ Aula_08_Laços_de_Repetição/senha.py
@@ -0,0 +1,9 @@
+# Pedindo denha até acertar
+
+senha_correta = "1234"     # senha correta
+senha = ""                 # pedir senha pro usuário
+
+while senha != senha_correta:
+    senha = input("Digite a senha: ")
+
+print("Acesso permitido!")
