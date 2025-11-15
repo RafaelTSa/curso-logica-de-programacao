cf991542ddd26ac66445cf3f103056bf5d187b1e
Rafael T Santos <texera09@gmail.com>
2025-11-15 19:59:17 -0300

    adiciona numeros_impares.py usando laço de repetição for

---
+++ Aula_08_Laços_de_Repetição/numeros_impares.py
@@ -0,0 +1,9 @@
+# números ímpares
+
+# range (início, fim, passo):
+# início: Onde a sequência começa.
+# fim: O número onde a sequência termina.
+# passo: O valor de incremento entre cada número.
+
+for numero in range (1, 21, 2):
+    print(numero)
