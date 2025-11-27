bdc4ac38a6f699988904fbe0f6b5f97932e03fd5
Rafael T Santos <texera09@gmail.com>
2025-11-26 23:12:16 -0300

    adiciona lista_alterando_valor.py

---
+++ Aula_09_Listas_e_Tuplas/lista_alterando_valor.py
@@ -0,0 +1,5 @@
+# alterando um valor na lista
+frutas = ["maça", "banana", "Laranja"]
+
+frutas[1] = "uva"  # altera o item na ordem 1 da lista "banana" e altera para "uva, lista começa do 0, 1, 2"
+print(frutas)       # mostra lista com a alteração
