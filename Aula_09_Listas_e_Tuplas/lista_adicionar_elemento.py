a25f033f4a6af277a3f7ca620b21bfe73a26e30b
Rafael T Santos <texera09@gmail.com>
2025-11-27 21:27:23 -0300

    adiciona comentario e print para mostrar lista antes do append

--- Aula_09_Listas_e_Tuplas/lista_adicionar_elemento.py
+++ Aula_09_Listas_e_Tuplas/lista_adicionar_elemento.py
@@ -4,5 +4,9 @@
 # insert(): adiciona um item em uma posição específica
 
 numeros = [1, 2, 3]
-numeros.append(4)
 print(numeros)
+
+numeros.append(4)
+print(numeros) # [1, 2, 3, 4] após o append adiciona o 4 a lista.
+
+
