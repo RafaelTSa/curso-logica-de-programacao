e7258bf624727f6c258d8c57b59bb8258a226a32
Rafael T Santos <texera09@gmail.com>
2025-11-27 21:34:20 -0300

    adiciona comentario ao insert

--- Aula_09_Listas_e_Tuplas/lista_adicionar_elemento.py
+++ Aula_09_Listas_e_Tuplas/lista_adicionar_elemento.py
@@ -10,5 +10,5 @@ numeros.append(4)
 print(numeros) # [1, 2, 3, 4] após o append adiciona o 4 a lista.
 
 
-numeros.insert(1, 10)
-print(numeros)
+numeros.insert(1, 10) # (posição, valor)
+print(numeros) # [1, 10, 2, 3, 4] (insere o nuemro 10 na posição 1 da lista numeros)
