5997fc17ecb5b699c2db672d0cd79a684c7c2021
Rafael T Santos <texera09@gmail.com>
2025-11-27 21:08:48 -0300

    adiciona comentario sobre append e insert

--- Aula_09_Listas_e_Tuplas/lista_adicionar_elemento.py
+++ Aula_09_Listas_e_Tuplas/lista_adicionar_elemento.py
@@ -1,5 +1,8 @@
 # adicionado elementos à lista
 
+# append(): adiciona um item ao final
+# insert(): adiciona um item em uma posição específica
+
 numeros = [1, 2, 3]
 numeros.append(4)
 print(numeros)
