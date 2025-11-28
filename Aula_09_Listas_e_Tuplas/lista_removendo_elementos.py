bbf864cb9b50a896990f1068d575a2f67d92de69
Rafael T Santos <texera09@gmail.com>
2025-11-27 22:51:06 -0300

    adiciona comentario sobre Remove e Pop, e gera lista frutas

--- Aula_09_Listas_e_Tuplas/lista_removendo_elementos.py
+++ Aula_09_Listas_e_Tuplas/lista_removendo_elementos.py
@@ -1,1 +1,6 @@
 # Removendo elementos da lista
+
+# Remove(): remove um item da lista
+# pop(): remove um item pelo índice (ou o último item se nenhum "for" passado)
+
+frutas = ["maça", "banana", "laranja"]
