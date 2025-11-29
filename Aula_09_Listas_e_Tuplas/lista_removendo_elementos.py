a795388909570d5af0c653106b5a23dba7b0d3d7
Rafael T Santos <texera09@gmail.com>
2025-11-28 21:10:06 -0300

    adiciona frutas.remove e frutas.pop

--- Aula_09_Listas_e_Tuplas/lista_removendo_elementos.py
+++ Aula_09_Listas_e_Tuplas/lista_removendo_elementos.py
@@ -3,4 +3,9 @@
 # Remove(): remove um item da lista
 # pop(): remove um item pelo índice (ou o último item se nenhum "for" passado)
 
-frutas = ["maça", "banana", "laranja"]
+frutas = ["maça", "banana", "laranja", "uva"]
+frutas.remove("banana")
+print(frutas) # ["maça", "laranja"]
+
+frutas.pop(0)
+print(frutas) # ["laranja"] apenas "laranja" aparece ja que o .remove retirou o "banana" antes.
