5ecf60c1b6105f1d2a9e535d4916dc766b783cd8
Rafael T Santos <texera09@gmail.com>
2025-12-08 16:16:47 -0300

    adiciona print para acessar elementos da tupla

--- Aula_09_Listas_e_Tuplas/tuplas.py
+++ Aula_09_Listas_e_Tuplas/tuplas.py
@@ -6,3 +6,9 @@ numeros = (1, 2, 3, 4, 5)
 
 print(cores)
 print(numeros)
+
+# Acessando elementos
+print(cores[0]) # "vermelho"
+print (cores[-1]) # verde
+print(numeros[2]) # 3
+print(numeros[-2]) # 4
