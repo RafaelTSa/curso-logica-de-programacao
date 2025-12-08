6b4c7f84cffc2606c287b90706dc84044c73fcfb
Rafael T Santos <texera09@gmail.com>
2025-12-08 16:09:42 -0300

    adiciona tuplas.py

---
+++ Aula_09_Listas_e_Tuplas/tuplas.py
@@ -0,0 +1,8 @@
+# Tuplas
+# Tuplas são como listas, mas imutáveis. Elas são criadas com parênteses ( ).
+
+cores = ("vermelho", "azul", "verde")
+numeros = (1, 2, 3, 4, 5)
+
+print(cores)
+print(numeros)
