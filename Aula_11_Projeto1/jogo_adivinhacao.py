c46cb9a93ca9e7eebf6b9e190a4b436c7f8c878d
Rafael T Santos <texera09@gmail.com>
2025-12-17 10:24:32 -0300

    adiciona variaveis e estrutura ao jogo_adivinhacao.py

--- Aula_11_Projeto1/jogo_adivinhacao.py
+++ Aula_11_Projeto1/jogo_adivinhacao.py
@@ -1,4 +1,16 @@
 # Jogo de adivinhação
 
-# No jogo o usuário precisa adivinhar um número secreto.
+# No jogo, o usuário precisa adivinhar um número secreto.
 # Ele pode tentar várias vezes até acertar.
+
+numero_secreto = 6
+tentativa = 0
+
+while tentativa != numero_secreto:
+    tentativa = int(input("Tente adivinhar um número de 1 a 10: "))
+    if tentativa < numero_secreto:
+        print("O número secreto é maior que", tentativa)
+    elif tentativa > numero_secreto:
+        print("O número secreto é menor que", tentativa)
+else:
+    print("Parabéns! Você acertou!")
