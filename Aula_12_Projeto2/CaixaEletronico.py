cf849b09f595b42e15250826eea1191bfab556c1
Rafael T Santos <texera09@gmail.com>
2026-01-04 13:18:31 -0300

    Adiciona variavel e estrutura de repetição ao programa

--- Aula_12_Projeto2/CaixaEletronico.py
+++ Aula_12_Projeto2/CaixaEletronico.py
@@ -1,5 +1,20 @@
 # Simulando um Caixa Eletronico
 
 # O Usuário tem um saldo inicial de R$ 500 e pode
-# Sacra dinheiro até zerar o saldo ou encerrar
+# Sacar dinheiro até zerar o saldo ou encerrar
 
+saldo = 500
+
+while saldo > 0:
+    saque = float(input("Informe o valor do saque (ou 0 para encerrar):"))
+
+    if saque == 0:
+        print("Saque encerrado.")
+        break
+
+    if saque > saldo:
+        print("Saldo insuficiente! Saque não efetuado.")
+    else:
+        saldo -= saque
+        print(f"Saque efetuado! Novo saldo R$ {saldo:.2f}")
+        
