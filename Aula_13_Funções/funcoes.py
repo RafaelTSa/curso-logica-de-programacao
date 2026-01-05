2cd1907dae72f0d4346d5736f3ab16e91201e188
Rafael T Santos <texera09@gmail.com>
2026-01-05 20:48:12 -0300

    adiciona funcoes.py a o diretorio da Aula_13_Funções

---
+++ Aula_13_Funções/funcoes.py
@@ -0,0 +1,10 @@
+# Funções
+
+# Funções são blocos de código reutilizáveis que realizam
+# uma tarefa específicca. Em vez de escrever o mesmo código
+# várias vezes, criamos uma função e apenas a chamamos sempre que necessaário.
+
+# Exemplo "real"
+# Imagine que você tem que calcular o imposto de vários produtos em uma loja.
+# Em vez de repetir a mesma conta várias vezes, vovê pode criar uma função
+# chamada calcular_imposto() e usá-la sempre que presisar.
