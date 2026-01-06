915b0dd91c50080c13774e31e02ad0686d064c38
Rafael T Santos <texera09@gmail.com>
2026-01-06 08:51:55 -0300

    adiciona função saudacao

--- Aula_13_Funções/funcoes.py
+++ Aula_13_Funções/funcoes.py
@@ -8,3 +8,6 @@
 # Imagine que você tem que calcular o imposto de vários produtos em uma loja.
 # Em vez de repetir a mesma conta várias vezes, vovê pode criar uma função
 # chamada calcular_imposto() e usá-la sempre que precisar.
+
+def saudacao(nome):
+    print(f"Olá, {nome}! Bem-vindo ao curso de Python.")
