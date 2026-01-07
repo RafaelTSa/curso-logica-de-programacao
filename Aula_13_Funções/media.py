1bd8110c3138cb81570134cec801a24e6842b5f7
Rafael T Santos <texera09@gmail.com>
2026-01-07 12:21:10 -0300

    adiciona media.py com a função calcular_media

---
+++ Aula_13_Funções/media.py
@@ -0,0 +1,9 @@
+# Função com vários parametros
+
+def calcular_media(n1, n2, n3):
+    media = (n1 + n2 + n3) / 3
+    return media
+
+# Chamando a função
+resultado = calcular_media(8, 9, 7)
+print(f"A média é {resultado:.2f}")
