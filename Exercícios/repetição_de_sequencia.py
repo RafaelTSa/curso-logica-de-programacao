bce36eea712bbea940f3e462cd19528f6ce3c2a9
Rafael T Santos <texera09@gmail.com>
2026-01-12 10:08:31 -0300

    adiciona repetição_de_sequencia.py

---
+++ Exercícios/repetição_de_sequencia.py
@@ -0,0 +1,12 @@
+def repeat_str(repeat, string):
+    return repeat * string
+
+repeat = int(input("Quantas vezes? "))
+string = input("Digite a palavra: ")
+
+resultado = repeat_str(repeat, string)
+
+print(resultado)
+
+
+
